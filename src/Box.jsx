510abffb53ee5a9b134331edb55152395a4b252e
Susan Pandey <susanpanday@SUSANs-MacBook-Air.local>
2025-01-30 11:50:31 +0545

    form created

---
+++ src/Box.jsx
@@ -0,0 +1,8 @@
+<form action="">
+  <div>
+    <h1 className="text-4xl">Login Here</h1>
+  </div>
+  <div>
+    <textarea name="userName" id="" placeholder="username"></textarea>
+  </div>
+</form>;
